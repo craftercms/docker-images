afd94f4926794b29b2a666d48dc26ec11dd47833
Phil Nguyen <nquangphuong@gmail.com>
2023-10-12 04:36:40 +0700

    Update docker-entryproint.sh to start without root (#79)
    * Update docker-entryproint.sh to start without root
    
    * Only change apache2 folder ownership for git_https_server
    
    * Use crafter for logrotate
    
    * Update ssh and https server to use /home/crafter as the application folder

--- images/logrotate/docker-entrypoint.sh
+++ images/logrotate/docker-entrypoint.sh
@@ -14,10 +14,5 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-cat logrotate.tpl.conf | envsubst > /etc/logrotate.conf
-
-# Setup crafter user crontab
-echo "$CRON_SCHEDULE /usr/sbin/logrotate /etc/logrotate.conf" | crontab -
-
 # Run crontab through `tini`
 exec tini $@
