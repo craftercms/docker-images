de3766318cb6c5e38bbcc05c6658b7f0dbde9f2e
Sumer Jabri <sumer.jabri@craftercms.org>
2024-01-26 14:56:31 -0500

    WIP

--- delete-image.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TAG=$1
-
-docker image rm -f craftercms/authoring_tomcat:$TAG craftercms/authoring_local:$TAG craftercms/delivery_tomcat:$TAG craftercms/deployer:$TAG craftercms/git_ssh_server:$TAG craftercms/git_https_server:$TAG craftercms/logrotate:$TAG
